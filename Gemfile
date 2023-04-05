94cb6b9393c69b261ba19c6d400a0e188cd3d28e
nagato-yt <nagatoyutoyuto@gmail.com>
2023-04-05 11:50:37 +0900

    devise日本語化

--- Gemfile
+++ Gemfile
@@ -72,4 +72,5 @@ group :test do
 end
 
 gem 'devise'
+gem 'devise-i18n'
 
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,5 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %m月 %d日 %H時 %M分'
+
+# 書き方
+# <%= xxx.created_at.to_s(:datetime_jp) %>
+# <%= xxx.updated_at.to_s(:datetime_jp) %>
