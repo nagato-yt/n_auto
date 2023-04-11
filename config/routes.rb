cd2a31f7da09532ae5a1d2a85d6f7cc06f02157a
nagato-yt <nagatoyutoyuto@gmail.com>
2023-04-11 23:36:52 +0900

    controllor model 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  devise_for :admins
   get 'homes/top'
   root 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
